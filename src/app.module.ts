381805ecd351ffc21e86c4c0421165a74220350a
Tobi Adeniji <adenijitobi94@gmail.com>
2024-11-20 23:02:35 +0100

    set swagger for doucmentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { FxqlModule } from './fxql/fxql.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ApiKeyGuard } from './common/guards/api-key.guard';
 
 @Module({
-  imports: [DatabaseModule, FxqlModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ThrottlerModule.forRoot([{
+      ttl: 60,
+      limit: 10
+    }]),
+    DatabaseModule, 
+    FxqlModule
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ApiKeyGuard
+    }
+  ],
 })
+
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(helmet());
+
+  dotenv.config();
+
+  const config = new DocumentBuilder()
+    .setTitle('Foreign Exchange Query Language Parser')
+    .setDescription('API documentation for FXQL service')
+    .setVersion('1.0')
+    .addTag('fxql')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+
+  await app.listen(process.env.PORT || 5000 );
 }
 bootstrap();
